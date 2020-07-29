f9069d798f7349ac882e558851fb93caaf1501db
Adi Baswendra <cah.sibat@gmail.com>
2020-07-29 14:58:04 +0700

    remove redux-logger

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 import rootReducer from './reducers/rootReducer';
-const middleware = applyMiddleware(thunk,logger);
+const middleware = applyMiddleware(thunk);
 const store = createStore(rootReducer, middleware);
 export { store };
