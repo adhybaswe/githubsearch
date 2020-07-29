6b72ffe5d15117c0c30f1bea7dfbe72e2e38cbe8
Adi Baswendra <cah.sibat@gmail.com>
2020-07-29 15:19:49 +0700

    Updates

---
+++ precache-manifest.efa8c4fa50fd02ef69ce5622c59626fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cbbd1e7c7d204dde3eb9179204fad50",
+    "url": "/githubsearch/index.html"
+  },
+  {
+    "revision": "33f1d0107cfa1c835a2e",
+    "url": "/githubsearch/static/css/main.c7f9c92c.chunk.css"
+  },
+  {
+    "revision": "f5b9e23836e313ca17f2",
+    "url": "/githubsearch/static/js/2.00c52a7b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/githubsearch/static/js/2.00c52a7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33f1d0107cfa1c835a2e",
+    "url": "/githubsearch/static/js/main.9a1e6106.chunk.js"
+  },
+  {
+    "revision": "06196d2c93a5f84fc022",
+    "url": "/githubsearch/static/js/runtime-main.1c13ee10.js"
+  }
+]);
